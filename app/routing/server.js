054aec91f7e13d440c825789f6dae9f3424d9e62
mrderrickguy <zakiahdg@gmail.com>
2018-03-03 13:48:08 -0600

    update server

--- app/routing/server.js
+++ app/routing/server.js
@@ -1,7 +1,7 @@
 // Dependencies
 var bodyParser = require('body-parser');
 var express = require('express');
-var PORT = process.env.PORT||8080;
+var PORT = process.env.PORT||3000;
  
 var app = express()
  
